601db06f47b8d49db8ed2ce1eb0cd77e402a2d77
Tomi Kasurinen <tomikasu@dhcp-asv-72.helsinki.fi>
2017-05-14 14:50:36 +0300

    Added more of Chapter 9.

--- app/Chap9.hs
+++ app/Chap9.hs
@@ -1,6 +1,6 @@
 module Chap9 where
 
-import Data.Char (isSpace)
+import Data.Char
 
 -- Exercise: EnumFromTo
 -- instead of doing etfBool, etfOrd, etfInt & etfChar I made myEtf that works for all of those.
@@ -57,3 +57,159 @@ myWords' w = mySplitter ' ' w
 
 myLines' w = mySplitter '\n' w
 
+-- Exercises: Comprehend Thy Lists
+
+mySqr = [x^2 | x <- [1..5]]
+
+--- only even numbers
+what1 = [x | x <- mySqr, rem x 2 == 0]
+
+-- empty list, as no y satisfies the filter condition
+what2 = [(x, y) | x <- mySqr, y <- mySqr, x < 50, y > 50]
+
+-- empty list, as no y satisfies the filter condition
+what3 = take 5 [ (x, y) | x <- mySqr, y <- mySqr, x < 50, y > 50 ]
+
+-- Exercises: Square Cube
+
+myCube = [y^3 | y <- [1..5]]
+
+-- 1.
+sqrsAndCubes = [(x, y) | x <- mySqr, y <- myCube]
+
+-- 2.
+sqrsAndCubesFilt1 = [(x, y) | x <- mySqr, y <- myCube, x < 50, y < 50]
+
+-- 3.
+sqrsAndCubesFilt2 = take 4 [(x, y) | x <- mySqr, y <- myCube, x < 50, y < 50]
+
+
+-- Exercises: Bottom Madness
+
+-- will blow
+blow1 = [x^y | x <- [1..5], y <- [2, undefined]]
+
+-- will not
+blow2 = take 1 $ [x^y | x <- [1..5], y <- [2, undefined]]
+
+-- will blow
+blow3 = sum [1, undefined, 3]
+
+-- will not (only uses the spine)
+blow4 = length [1, 2, undefined]
+
+-- will blow
+blow5 = length $ [1, 2, 3] ++ undefined
+
+-- will not (wrong guess)
+blow6 = take 1 $ filter even [1, 2, 3, undefined]
+
+-- will blow this time!
+blow7 = take 1 $ filter even [1, 3, undefined]
+
+-- will not
+blow8 = take 1 $ filter odd [1, 3, undefined]
+
+-- will not
+blow9 = take 2 $ filter odd [1, 3, undefined]
+
+-- will blow
+blow10 = take 3 $ filter odd [1, 3, undefined]
+
+-- Exercises: More Bottoms
+
+-- will blow
+blow11 = take 1 $ map (+1) [undefined, 2, 3]
+
+-- will not
+blow12 = take 1 $ map (+1) [1, undefined, 3]
+
+-- will blow
+blow13 = take 2 $ map (+1) [1, undefined, 3]
+
+-- 4.
+itIsMystery :: [Char] -> [Bool]--
+itIsMystery xs = map (\x -> elem x "aeiou") xs
+
+-- 5.
+
+whatA = map (^2) [1..10]
+-- [1, 4, 9, 16, 25, 36, 49, 64, 81, 100
+
+whatB = map minimum [[1..10], [10..20], [20..30]]
+-- [1, 10, 20]
+
+whatC = map sum [[1..5], [1..5], [1..5]]
+-- [15, 15, 15]
+
+-- Exercises: Filtering
+
+-- 1.
+mult3 = filter (\x -> rem x 3 == 0) [1..30]
+
+-- 2.
+cntMult3 = length $ filter (\x -> rem x 3 == 0) [1..30]
+
+-- 3.
+
+toFilter = "the brown dog was a goof"
+
+myFilter :: [Char] -> [[Char]]
+myFilter s = filter notArticle ws
+  where isThe = \w -> (==) "the" w || (==) "The" w
+        isA   = \w -> (==) "a" w || (==) "A" w
+        isAn  = \w -> (==) "an" w || (==) "An" w
+        isArticle = \w -> (isThe w) || (isA w) || (isAn w)
+        notArticle = not . isArticle
+        ws = words s
+
+-- Zipping exercises
+
+-- 1.
+myZip :: [a] -> [b] -> [(a, b)]
+myZip [] _ = []
+myZip _ [] = []
+myZip (x:xs) (y:ys) = (x, y) : (myZip xs ys)
+
+-- 2.
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith _ [] _ = []
+myZipWith _ _ [] = []
+myZipWith f (x:xs) (y:ys) = (f x y) : (myZipWith f xs ys)
+
+-- 3.
+myZip' xl yl = myZipWith (\x y -> (x, y)) xl yl
+
+-- Chapter Exercises
+
+-- Data.Char
+
+-- 1.
+-- isUpper :: Char -> Bool 	-- Defined in ‘GHC.Unicode’
+-- toUpper :: Char -> Char 	-- Defined in ‘GHC.Unicode’
+
+-- 2.
+badHello = "HbEfLrLxO,"
+
+filtUpper :: [Char] -> [Char]
+filtUpper s = [x | x <- s, isUpper x]
+
+-- 3.
+capFirst :: [Char] -> [Char]
+capFirst [] = []
+capFirst (x:xs) = toUpper x : xs
+
+-- 4.
+capAll :: [Char] -> [Char]
+capAll [] = []
+capAll (x:xs) = toUpper x : capAll xs
+
+-- 5.
+capHead :: [Char] -> Char
+capHead l = toUpper . head $ l
+
+-- 6.
+capHead' :: [Char] -> Char
+capHead' = toUpper . head
+
+-- Ciphers in module Cipher.hs
