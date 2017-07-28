9784a79163dcf0255150b7acbb6e5fc7255b3230
Tomi Kasurinen <tomi.kasurinen@gmail.com>
2017-07-28 15:04:42 +0300

    Added BinaryTree exercises from Chapter 11.

--- app/Chap11.hs
+++ app/Chap11.hs
@@ -202,3 +202,86 @@ allProgrammers = [Programmer os lang | os <- allOperatingSystems, lang <- allLan
 
 programmerCountCheck :: Bool
 programmerCountCheck = (length allProgrammers) == (length allOperatingSystems * length allLanguages)
+
+
+-- Exericises: Binary Tree
+data BinaryTree a =
+    Leaf
+    | Node (BinaryTree a) a (BinaryTree a)
+    deriving (Eq, Ord, Show)
+
+insert' :: Ord a => a -> BinaryTree a -> BinaryTree a 
+insert' b Leaf = Node Leaf b Leaf
+insert' b (Node left a right)
+    | b == a = Node left a right
+    | b < a  = Node (insert' b left) a right 
+    | b > a  = Node left a (insert' b right)
+
+-- implement map for BinaryTree
+mapTree :: (a -> b) -> BinaryTree a -> BinaryTree b 
+mapTree _ Leaf = Leaf
+mapTree f (Node left a right) =
+    Node (mapTree f left) (f a) (mapTree f right)
+
+testTree' :: BinaryTree Integer
+testTree' =
+    Node (Node Leaf 3 Leaf) 1 (Node Leaf 4 Leaf) 
+    
+mapExpected =
+    Node (Node Leaf 4 Leaf) 2 (Node Leaf 5 Leaf)
+
+-- acceptance test for mapTree
+mapOkay =
+    if mapTree (+1) testTree' == mapExpected 
+    then print "yup okay!"
+    else error "test failed!"
+
+-- implement BinaryTree to Lists
+preorder :: BinaryTree a -> [a]
+preorder Leaf = []
+preorder (Node left a right) =
+    [a] ++ (preorder left) ++ (preorder right)
+
+inorder :: BinaryTree a -> [a]
+inorder Leaf = []
+inorder (Node left a right) =
+    (preorder left) ++ [a] ++ (preorder right)
+
+postorder :: BinaryTree a -> [a]
+postorder Leaf = []
+postorder (Node left a right) =
+    (preorder left) ++ (preorder right) ++ [a]
+
+testTree :: BinaryTree Integer
+testTree = Node (Node Leaf 1 Leaf) 2 (Node Leaf 3 Leaf)
+
+testPreorder :: IO () 
+testPreorder =
+    if preorder testTree == [2, 1, 3] 
+    then putStrLn "Preorder fine!" 
+    else putStrLn "Bad news bears."
+
+testInorder :: IO () 
+testInorder =
+    if inorder testTree == [1, 2, 3] 
+    then putStrLn "Inorder fine!" 
+    else putStrLn "Bad news bears."
+
+testPostorder :: IO () 
+testPostorder =
+    if postorder testTree == [1, 3, 2] 
+    then putStrLn "Postorder fine!"
+    else putStrLn "postorder failed check"
+
+--main :: IO () main = do
+-- mockMain function to test BinaryTree a -> [a] functions
+mockMain :: IO ()
+mockMain = do
+  testPreorder
+  testInorder
+  testPostorder
+
+-- implement fold for BinaryTree a (any traversal order)
+-- this might be a bit cheating?
+foldTree :: (a -> b -> b) -> b -> BinaryTree a -> b
+foldTree f z bTree = foldr f z (inorder bTree) 
